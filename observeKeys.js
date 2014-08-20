a0f91d9474fdcd60b6899b4068fd04ca154ffe8e
Andrew Page <andrew@fuzzy-logic.co.za>
2014-08-20 21:42:34 +0200

    Something Like

---
+++ observeKeys.js
@@ -0,0 +1,22 @@
+Obect.prototype.observeKeys = function(obj, prefix) {
+	prefix = prefix || '';
+	for (var key in obj) {
+		if (( typeof obj[key] != "function") && XOR(Object.prototype.toString.call(obj[key]) === '[object Array]', typeof obj[key] != "object")) {
+			obj.watch(key, function(p, was, now) {
+				var settings = {
+					"detail" : {
+						"prop" : p,
+						"now" : now,
+						"was" : was
+					},
+					bubbles : false,
+					cancelable : true
+				};
+				document.dispatchEvent(new CustomEvent((prefix + prop, settings));
+				return now;
+			});
+		}
+		if (obj[key] instanceof Object)
+			this.observeKeys(obj[key], prefix + key + '.');
+	}
+}
